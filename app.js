b3ae60c404234c65e81f83d48571515b12de6aa6
Eric Tanaka <cirept612@gmail.com>
2018-03-23 22:58:40 -0700

    cleaned up files
    moved files into the root

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-/**
- * This is an example of a basic node.js script that performs
- * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
- */
-
 let express = require('express'); // Express web server framework
 let request = require('request'); // "Request" library
 let querystring = require('querystring');
